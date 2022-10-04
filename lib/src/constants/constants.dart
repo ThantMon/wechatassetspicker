0c5ea10ad3e9f7cfede63d315571a650c2a25b91
tmn <thantmonnaing@cloudsource.co.jp>
2022-10-04 11:31:52 +0630

    add wechat asset

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,14 @@
+// Copyright 2019 The FlutterCandies author. All rights reserved.
+// Use of this source code is governed by an Apache license that can be found
+// in the LICENSE file.
+
+import 'package:flutter/painting.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+/// Default theme color from WeChat.
+const Color defaultThemeColorWeChat = Color(0xff00bc56);
+
+const ThumbnailSize defaultAssetGridPreviewSize = ThumbnailSize.square(200);
+const ThumbnailSize defaultPathThumbnailSize = ThumbnailSize.square(80);
+
+const TextStyle defaultTextStyle = TextStyle(fontFamily: "Noto_Sans_JP");
